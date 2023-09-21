394e9003d1e391d38f6b5154b7545abfef214962
siam786e <shohrab.hossain36@gmail.com>
2023-09-21 20:21:13 +0600

    add product

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,42 @@
-<script setup></script>
+<script setup>
+import axios from "axios";
+import { onBeforeMount, ref } from "vue";
+import { RouterLink } from "vue-router";
+
+let products = ref([]);
+
+onBeforeMount(() => {
+  axios
+    .get("https://dummyjson.com/products/category/smartphones?limit=20")
+    .then((res) => {
+      products.value = res.data;
+    });
+});
+</script>
 
 <template>
-  
-
   <!-- Start Hero Section -->
   <div class="hero">
     <div class="container">
       <div class="row justify-content-between">
         <div class="col-lg-5">
           <div class="intro-excerpt">
-            <h1>Modern Interior <span clsas="d-block">Design Awesome</span></h1>
+            <h1>Outstanding Design <span clsas="d-block">Slim Design</span></h1>
             <p class="mb-4">
               Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
               velit. Aliquam vulputate velit imperdiet dolor tempor tristique.
             </p>
             <p>
-              <a href="" class="btn btn-secondary me-2">Shop Now</a
-              ><a href="#" class="btn btn-white-outline">Explore</a>
+              <router-link class="btn btn-secondary me-2" to="/shop"
+                >Shop Now</router-link
+              >
+              <a href="#" class="btn btn-white-outline">Explore</a>
             </p>
           </div>
         </div>
         <div class="col-lg-7">
           <div class="hero-img-wrap">
-            <img src="/images/couch.png" class="img-fluid" />
+            <img src="/images/one.jpg" class="img-fluid" />
           </div>
         </div>
       </div>
@@ -46,55 +60,25 @@
         <!-- End Column 1 -->
 
         <!-- Start Column 2 -->
-        <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-          <a class="product-item" href="cart.html">
-            <img
-              src="/images/iphone-13.png"
-              class="img-fluid product-thumbnail"
-            />
-            <h3 class="product-title">Nordic Chair</h3>
-            <strong class="product-price">$50.00</strong>
+        <div
+          v-for="(product, index) in products.products"
+          :key="index"
+          class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0"
+        >
+          <router-link
+            class="product-item"
+            :to="`/product-details/${product.id}`"
+          >
+            <img :src="product.thumbnail" class="img-fluid product-thumbnail" />
+            <h3 class="product-title">{{ product.title }}</h3>
+            <strong class="product-price">${{ product.price }}</strong>
 
             <span class="icon-cross">
-              <img src="/images/cross.svg" class="img-fluid" />
+              <p>View More</p>
             </span>
-          </a>
+          </router-link>
         </div>
         <!-- End Column 2 -->
-
-        <!-- Start Column 3 -->
-        <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-          <a class="product-item" href="cart.html">
-            <img
-              src="/images/product-2.png"
-              class="img-fluid product-thumbnail"
-            />
-            <h3 class="product-title">Kruzo Aero Chair</h3>
-            <strong class="product-price">$78.00</strong>
-
-            <span class="icon-cross">
-              <img src="/images/cross.svg" class="img-fluid" />
-            </span>
-          </a>
-        </div>
-        <!-- End Column 3 -->
-
-        <!-- Start Column 4 -->
-        <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-          <a class="product-item" href="cart.html">
-            <img
-              src="/images/product-3.png"
-              class="img-fluid product-thumbnail"
-            />
-            <h3 class="product-title">Ergonomic Chair</h3>
-            <strong class="product-price">$43.00</strong>
-
-            <span class="icon-cross">
-              <img src="/images/cross.svg" class="img-fluid" />
-            </span>
-          </a>
-        </div>
-        <!-- End Column 4 -->
       </div>
     </div>
   </div>
@@ -172,11 +156,7 @@
 
         <div class="col-lg-5">
           <div class="img-wrap">
-            <img
-              src="/images/samsung.jpg"
-              alt="Image"
-              class="img-fluid"
-            />
+            <img src="/images/samsung.jpg" alt="Image" class="img-fluid" />
           </div>
         </div>
       </div>
@@ -184,60 +164,5 @@
   </div>
   <!-- End Why Choose Us Section -->
 
-  
-
-  <!-- Start Popular Product -->
-  <div class="popular-product">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
-          <div class="product-item-sm d-flex">
-            <div class="thumbnail">
-              <img src="/images/iphone-13.png" alt="Image" class="img-fluid" />
-            </div>
-            <div class="pt-3">
-              <h3>Nordic Chair</h3>
-              <p>
-                Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio
-              </p>
-              <p><a href="#">Read More</a></p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
-          <div class="product-item-sm d-flex">
-            <div class="thumbnail">
-              <img src="/images/iphone-13.png" alt="Image" class="img-fluid" />
-            </div>
-            <div class="pt-3">
-              <h3>Kruzo Aero Chair</h3>
-              <p>
-                Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio
-              </p>
-              <p><a href="#">Read More</a></p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
-          <div class="product-item-sm d-flex">
-            <div class="thumbnail">
-              <img src="/images/iphone-13.png" alt="Image" class="img-fluid" />
-            </div>
-            <div class="pt-3">
-              <h3>Ergonomic Chair</h3>
-              <p>
-                Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio
-              </p>
-              <p><a href="#">Read More</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- End Popular Product -->
-
-
+ 
 </template>
